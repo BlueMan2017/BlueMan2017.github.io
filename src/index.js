4479746013bfc7c0ca408d7dbd1415f2aaf5e936
BlueMan2017 <s.k.tomaszewska@gmail.com>
2019-04-01 21:45:23 +0200

    postawiona strona

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import script from "./src/script.js";
+// import script from "./script.js";
 import "./scss/main.scss";
 
-script();
+// script();
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const postcss = require("postcss");
-const postcssPresetEnv = require("postcss-preset-env");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const distDir = path.resolve(__dirname, "dist");
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: `${__dirname}/dist/`,
-    filename: "out.js"
+    filename: "main.js",
+    path: distDir
   },
-  plugins: [new HtmlWebpackPlugin()],
-  watch: true,
-  mode: "production",
+  mode: "development",
+  devtool: "eval",
+  devServer: {
+    contentBase: distDir
+  },
   module: {
     rules: [
       {
@@ -21,19 +24,42 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/env"]
+            presets: ["@babel/preset-env"]
           }
         }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          isProduction
+            ? MiniCssExtractPlugin.loader
+            : { loader: "style-loader", options: { sourceMap: true } },
+          { loader: "css-loader", options: { sourceMap: isProduction } },
+          { loader: "postcss-loader", options: { sourceMap: isProduction } },
+          { loader: "sass-loader", options: { sourceMap: isProduction } }
+        ]
       }
     ]
   },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  }
-  // postcss([postcssPresetEnv(/* pluginOptions */)]).process(YOUR_CSS /*, processOptions */);
+  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
